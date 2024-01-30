62029450a312237a3d0c8023015968cf2fe3c4d3
amg-k <adamgorz@gmail.com>
2024-01-30 19:45:53 +0100

    Refactor. Change to nextInt() and add Scanner close()

--- Basic/Basic2.java
+++ Basic/Basic2.java
@@ -10,9 +10,10 @@ public class Basic2 {
         Scanner in = new Scanner(System.in);
         System.out.println("Program calculating sum of two numbers");
         System.out.println("Enter the first number:");
-        int firstNumber = Integer.parseInt(in.nextLine());
+        int firstNumber = in.nextInt();
         System.out.println("Enter the second number:");
-        int secondNumber = Integer.parseInt(in.nextLine());
+        int secondNumber = in.nextInt();
+        in.close();
         int sum = firstNumber + secondNumber;
         System.out.println("Sum of given numbers is: " + sum);
     }
