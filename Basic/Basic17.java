0b027eabac2b9908f5951ad03f105adb9ca8dc60
amg-k <adamgorz@gmail.com>
2024-02-01 14:57:40 +0100

    Add Scanner to input values

--- Basic/Basic17.java
+++ Basic/Basic17.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Basic17 {
     public static void main(String[] args) {
-        int firstNum = 0b10;
-        int secondNum = 0b11;
-        int sum = Math.addExact(firstNum, secondNum);
-
-        System.out.println(Integer.toBinaryString(sum));
+        System.out.println("Program calculating sum of two binary numbers");
+                
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input first binary number: ");
+        int firstBinary = Integer.bitCount(input.nextInt());
+        
+        System.out.print("Input second binary number: ");
+        int secondBinary = Integer.bitCount(input.nextInt());
+        input.close();
+        
+        System.out.println(Math.addExact(firstBinary, secondBinary));
     }
 }
